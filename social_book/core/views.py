068b8c1fdb5f33bab91ded816c2384ff86647583
nfebrian13 <nanaz.febrian@gmail.com>
2022-11-06 12:22:14 +0700

    create page login

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -45,5 +45,23 @@ def signup(request):
         return render(request, 'signup.html')
 
 
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+
 def index2(request):
     return HttpResponse('<h1>Welcome to Social Book</h1>')
